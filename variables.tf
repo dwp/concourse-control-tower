8bb2d52119d22b2a3c91264e02561f51298c9b31
Luke Bradbury <luke@nqyr.io>
2019-06-05 14:48:02 +0100

    Add basic web instances

--- variables.tf
+++ variables.tf
@@ -1,6 +1,23 @@
+# TODO: descriptions
 variable "cidr_block" {
   type = string
 }
 variable "tags" {
   type = map
+}
+
+variable "web_count" {
+  type = number
+}
+
+variable "concourse_version" {
+  type = string
+}
+
+variable "dns_zone_name" {
+  type = string
+}
+
+variable "whitelist_cidr_blocks" {
+  type = list(string)
 }
