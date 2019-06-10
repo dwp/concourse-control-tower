cf458845e809466d6ba264a7697ff661a4dbb0c2
Luke Bradbury <luke@nqyr.io>
2019-06-10 12:50:41 +0100

    Set descriptions and types

--- variables.tf
+++ variables.tf
@@ -1,40 +1,59 @@
-# TODO: descriptions
 variable "cidr_block" {
+  description = "cidr block to use for vpc"
   type = string
 }
 variable "tags" {
-  type = map
+  description = "tags to apply to aws resource"
+  type = map(string)
 }
 
 variable "cluster_name" {
+  description = "cluster name, used in dns"
   type        = string
   default     = "concourse"
-  description = "cluster name, used in dns"
 }
 
 variable "concourse_version" {
+  description = "concourse version to install"
   type        = string
   default     = "5.2.0"
-  description = "concourse version to install"
 }
 
 variable "parent_domain_name" {
-  type        = string
   description = "parent domain name for CI"
+  type        = string
 }
 
 variable "whitelist_cidr_blocks" {
+  description = "list of allowed cidr blocks"
   type = list(string)
 }
 
 variable "database" {
-  type = map
+  description = "database configuration options"
+  type = object({
+    name = string
+    user          = string
+    password      = string
+    instance_type = string
+    count         = number
+  })
 }
 
 variable "web" {
-  type = map
+  description = "atc/tsa configuration options"
+  type = object({
+    admin_user     = string
+    admin_password = string
+    count          = number
+    instance_type  = string
+  })
 }
 
 variable "worker" {
-  type = map
+  description = "worker configuration options"
+  type = object({
+    instance_type = string
+    count         = number
+  })
 }
